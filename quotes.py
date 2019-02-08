884b1bb6131dbaca39ca0f54644aa952fc0c1a6d
andresshare <andresshare@gmail.com>
2019-02-07 22:24:07 -0500

    config initial

--- quotes.py
+++ quotes.py
@@ -2,3 +2,7 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 quotes_pages = 'https://bluelimelearning.github.io/my-fav-quotes/'
+uClient = uReq(quotes_pages)
+page_html uClient.read()
+uClient.close()
+page_soup = soup(page_html,"html.parser")
