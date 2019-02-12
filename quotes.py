c02b15c5e7048707bf79a7b93a22b67aa16f5f95
andresshare <andresshare@gmail.com>
2019-02-11 22:38:09 -0500

    modified structure

--- quotes.py
+++ quotes.py
@@ -6,3 +6,14 @@ uClient = uReq(quotes_pages)
 page_html uClient.read()
 uClient.close()
 page_soup = soup(page_html,"html.parser")
+quotes = page_soup.find("div",{"class":"quotes"})
+
+for quotes in quotes:
+	fav_quotes = quote.findAll("p",{"class":"aquote"})
+	aquote = fav_quote[0].text.strip()
+
+	fav_authors = quote.findAll("p",{"class":author"})
+	author = fav_authors[0].text.strip()
+
+	print(quote)
+	print(author) 
